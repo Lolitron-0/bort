7bbb01046095ec7a1fa33fc47f1be3634ed43507
Lolitron-0 <ovcharov_05@mail.ru>
2024-10-23 12:08:12 +0300

    remove flags from CMakeLists

--- dev.sh
+++ dev.sh
@@ -35,7 +35,7 @@ cmake -S . \
 			-G Ninja \
 			-DCMAKE_CXX_COMPILER=clang++ \
 			-DCMAKE_C_COMPILER=clang \
-      -DCMAKE_CXX_FLAGS="-fsanitize=address,undefined,leak" \
+      -DCMAKE_CXX_FLAGS="-fsanitize=address,undefined,leak -Wall -Wextra -pedantic" \
 			-DCMAKE_BUILD_TYPE=Debug \
 			-DCMAKE_EXPORT_COMPILE_COMMANDS=1 
 cmake --build build --parallel $(nproc)

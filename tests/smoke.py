c7ac49b20d8957900d9536a8e1aba3283a608cc8
Lolitron-0 <ovcharov_05@mail.ru>
2025-02-04 13:16:39 +0300

    try fix ci

--- tests/smoke.py
+++ tests/smoke.py
@@ -4,7 +4,7 @@ import subprocess
 
 RED, GREEN, NC = '\033[0;31m', '\033[0;32m', '\033[0m'
 root = Path(__file__).parent.parent
-build_dir = sys.argv[1]
+build_dir = Path(sys.argv[1])
 corpus_dir = root / 'tests/corpus'
 exe_suffix = sys.argv[2] if len(sys.argv) > 2 else ''
 
@@ -12,7 +12,7 @@ for file in build_dir.glob("*"):
     print(file)
 
 for file in corpus_dir.glob('*.c'):
-    arg0 = Path(build_dir) / ('bort'+exe_suffix)
+    arg0 = build_dir / ('bort'+exe_suffix)
     print(arg0)
     result = subprocess.run(
         [arg0, '--dump-ast', '--emit-ir', file])
